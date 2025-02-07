e59c3ff07488599173350df0055a36ace3aa487f
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2025-02-07 19:49:17 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,4 @@
-
 USE alx_book_store;  
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE  
-FROM INFORMATION_SCHEMA.COLUMNS  
-WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
+INSERT INTO Customers (customer_id, customer_name, email, address)   
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
